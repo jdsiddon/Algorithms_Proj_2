4902b3f3c0f49b8ee7929471c7ae73a4054bf094
justin <justin@delaris.com>
2016-04-23 09:24:57 -0700

    Error in dynamic programming code.

--- index.js
+++ index.js
@@ -4,84 +4,31 @@
 // A = 15
 // C = [1, 1, 1, 1], i.e., 1, 1 cent, 1, 2 cent, 1, 4 cent, 1, 8 cent
 
-function changeslow(cents, coins, minCoins) {
-  var change = new Array();
-  var i;
-  // To make change for A cents, start by letting K = A
+function changeslow(cents, coins) {
 
-  // No cents, so no coins.
-  if(cents === 0) {
-    change.push(0);
-    change.push(0);
-    change.push(0);
-    change.push(0);
-    return change;
-  }
+  var i, j, k;
 
-  // If there is a K-cent coin, then that one coin is the minimum
-  for(i = 0; i < coins.length; i++) {
-    var coinFound = false;        // Flag if we found a coin of sufficient value.
-
-    if(cents === coins[i]) {
-      change.push(1);
-    } else {
-      change.push(0);
-    }
-
-    if(coinFound === true) {
-      return change;
+  for(i = 0; i < cents; i++) {
+    for(j = 0; j < cents; j++) {
+      for(k = 0; k < coins.length; k++) {
+        console.log(i, j, k);
+      }
     }
   }
+  // 1,2,3
 
-  // Otherwise, for each value i < K,
-  //
-  // 1) Find the minimum number of coins needed to make i cents
-  // Divide problem
-  var reducedChange = cents;
-  var currentMin;
+  // 1,1,1
+  // 1,2,2
+  // 2,2,2
+  // 2,2,3
+  // 2,3,3
+  // 3,3,3
 
-  // if(cents === minCoins) {
-  //   currentMin = cents;
-  // }
 
-  // for(i = 0; i < cents; i++) {
-  //   var bottom = changeslow(i, coins, minCoins);
-  //   var top = changeslow(cents-i, coins, minCoins);
-  //
-  //   var totalCoins = bottom.concat(test2).reduce(function(prev, curr) {
-  //     return prev + curr;     // Add previous element to current element.
-  //   });
-  //
-  //   if(totalCoins < minCoins) {
-  //     minCoins = totalCoins;
-  //   }
-  //
-  //   console.log(bottom, top);
-  // }
 
-  // while(reducedChange > 0) {
-  //   reducedChange-=1;
-  //   var bottom = changeslow(reducedChange, coins, currentMin);
-  //   var top = changeslow(cents-reducedChange, coins, currentMin);
-  //
-  //   // Combine top and bottom and sum the arrays. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-  //   var totalCoins = bottom.concat(test2).reduce(function(prev, curr) {
-  //     return prev + curr;     // Add previous element to current element.
-  //   });
-  //
-  //   if(totalCoins < minCents) {     // The divided problem returned a smaller sum of coins.
-  //     bottom.forEach(function(val, idx) {
-  //       change.push(val + top[idx]);
-  //     })
-  //
-  //     return change;
-  //   }
-  // }
-  // 2) Find the minimum number of coins needed to make K - i cents
-  //
-  // Choose the i that minimizes the sum of 1) and 2)
-  console.log(change);
-  return change;
+
+  // console.log(change);
+  // return change;
 
 }
 
@@ -124,7 +71,12 @@ function changegreedy(change, coins) {
 // is, how do you build C[i]?) This implementation is called changedp. Note: there are other versions of
 // the DP algorithm you may use one but need to explain in your report.
 
+
+// https://www.youtube.com/watch?v=Kf_M7RdHr1M
 function changedp(change, coins, minSequences) {
+  var tempChange = new Array(coins.length);     // Create a new array the same size as the coins available.
+  tempChange.fill(0);     // Each coin is selected 0 times to start.
+
   if(change === 0) {      // Change is 0, don't select any coins.
     return 0;
   }
@@ -136,11 +88,11 @@ function changedp(change, coins, minSequences) {
     }
   })
 
-  if(minChange > -1) {       // Smaller amount found.
+  if(minChange > -1) {       // A precalculated value already exists so return that instead of recalculating a value.
     return minChange;
   }
 
-  var min = Number.MAX_SAFE_INTEGER;
+  var min = Number.MAX_SAFE_INTEGER;    // Set a large value as the minimum number of coins to make change
 
   for(var i = 0; i < coins.length; i++) {
 
@@ -148,17 +100,18 @@ function changedp(change, coins, minSequences) {
       continue;     // The coin is to big to effect the number of coins required to give change.
     }
 
-    var val = changedp(change-coins[i], coins, minSequences);
+    var val = changedp(change-coins[i], coins, minSequences);     // Recurse with the total quantity of change minus current coin denom.
+    console.log("va: " + val);
 
-    if(val < min) {
+    if(val < min) {       // The amount returned from recursive call is less than the current minimum.
       min = val;
     }
 
   }
 
-  min = (min === Number.MAX_SAFE_INTEGER ? min : min + 1);
+  min = (min === Number.MAX_SAFE_INTEGER ? min : min + 1);        // Include first recurse.
 
-  minSequences.push({"change":change, "min":min});
+  minSequences.push({"change":change, "minCoins":min});                // Add object to array with change quantity and minimum number of coins.
   console.log(minSequences);
   return min;
 }
@@ -169,9 +122,11 @@ function changedp(change, coins, minSequences) {
 
 // changeslow(15, [1, 2, 4, 8], 15);
 changeslow(1, [1, 2, 4, 8], 1);
+changeslow(25, [1, 2, 4, 8], 25);
+
 
 console.log(changegreedy(0, [1, 2, 4, 8]));      // Should return 0.
 console.log(changegreedy(15, [1, 2, 4, 8]));     // Should be [1, 1, 1, 1]
-
+//
 console.log(changedp(0, [1, 2, 4, 8], new Array()));      // Should return 0.
 console.log(changedp(15, [1, 2, 4, 8], new Array()));     // Should be [1, 1, 1, 1]
