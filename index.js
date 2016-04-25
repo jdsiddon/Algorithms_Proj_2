53a92c6f3ce2e5b3c7c5a50a12182b56e6391fce
justin <justin@delaris.com>
2016-04-25 07:54:46 -0700

    PUlled from master.

--- index.js
+++ index.js
@@ -4,32 +4,38 @@
 // A = 15
 // C = [1, 1, 1, 1], i.e., 1, 1 cent, 1, 2 cent, 1, 4 cent, 1, 8 cent
 
-function changeslow(cents, coins, minChange) {
-  // if(cents < 0) { // No solution.
-  //   return 0;
-  // }
-  //
-  // if(cents == 0) { // Cents requested in 0, so only 1 solution exists.
-  //   return 1;
-  // }
-  //
-  // var sum = 0;
-  // for(var i = 0; i < coins.length; i++) {
-  //   sum+=changeslow(cents-coins[i], coins, 0)
-  // }
-  //
-  // // if(minChange == coins.length && cents > 0) {
-  //   return 0;      //
-  // }
-  //
-  // return changeslow(cents-coins[minChange], coins, minChange) + changeslow(cents, coins, minChange+1);
+function changeslow(cents, coins) {
+
+  var i, j, k;
+
+  for(i = 0; i < cents; i++) {
+    for(j = 0; j < cents; j++) {
+      for(k = 0; k < coins.length; k++) {
+        console.log(i, j, k);
+      }
+    }
+  }
+  // 1,2,3
+
+  // 1,1,1
+  // 1,2,2
+  // 2,2,2
+  // 2,2,3
+  // 2,3,3
+  // 3,3,3
+
+
+
+
+  // console.log(change);
+  // return change;
+
 }
-
-
 
 function changegreedy(change, coins) {
   var tempChange = new Array(coins.length);     // Create a new array the same size as the coins available.
-  tempChange.fill(0);
+  //tempChange.fill(0);
+  (tempChange = []).length = coins.length;
 
   if(change === 0) {                    // No change required.
     return tempChange;
@@ -67,68 +73,49 @@ function changegreedy(change, coins) {
 // the DP algorithm you may use one but need to explain in your report.
 
 
-// https://www.youtube.com/watch?v=rdI94aW6IWw
-// In the linked example minArray = c and lastCoinUsed = s;
-function changedp(change, coins, minCoins) {
-  var changeArr = new Array(coins.length);     // Create a new array the same size as the coins available.
-  changeArr.fill(0);
+// https://www.youtube.com/watch?v=Kf_M7RdHr1M
+function changedp(change, coins, minSequences) {
+  var tempChange = new Array(coins.length);     // Create a new array the same size as the coins available.
+  //tempChange.fill(0);     // Each coin is selected 0 times to start.
+  (tempChange = []).length = coins.length;
 
-  var minArray = [];        // Array to store minimum number of coins for each sub-change (change-n) where n is the index of the array.
-  var lastCoinUsed = [];    // Array to store the last coin used in determining the minimum coins required, needed to figure out how many of each quantity.
-  var j = 0;
-  var z = 0;
+  if(change === 0) {      // Change is 0, don't select any coins.
+    return 0;
+  }
 
-  // Loop through each possible sub-change value for change. (change-n);
-  for(var i = 0; i < change+1; i++) {
-    if(i === 0) {     // Sub-change = 0 so no possible coins, and last coin used is 0.
-      minArray[i] = 0;
-      lastCoinUsed[i] = 0;
+  // Check if minimum already exists (memoization), return that if it exists because we don't need to calculate it again.
+  var minChange = minSequences.findIndex(function(elem, idx){
+    if(elem.change === change) {
+      return elem;
+    }
+  })
 
-    } else {
+  if(minChange > -1) {       // A precalculated value already exists so return that instead of recalculating a value.
+    return minChange;
+  }
 
-      var minCoins = Number.MAX_SAFE_INTEGER;     // A huge number that will never actually be the minimum number of coins.
-      var k = 0;                                  // Iterator over our coins array.
+  var min = Number.MAX_SAFE_INTEGER;    // Set a large value as the minimum number of coins to make change
 
-      while(i >= coins[k] && k < coins.length) {  // As long as our sub-change value is less than or equal to a coin denom lets find the minimum number required.
-        if(minCoins > minArray[i-coins[k]]+1) {
-          minCoins = minArray[i-coins[k]]+1;
-        }
-        k++;
-      }
+  for(var i = 0; i < coins.length; i++) {
 
-      minArray[i] = minCoins;            // Set minium coins for subchange value.
-
-
-      // Figure out the last coin that was used in making the minimum coins.
-      z = k;
-      while((i % coins[z]) > 0) { // The last coin used will be the largest one the sub-value is divisible by.
-        z--;
-      }
-      lastCoinUsed[i] = coins[z];            // Store last coin used.
+    if(coins[i] > change) {
+      continue;     // The coin is to big to effect the number of coins required to give change.
     }
 
+    var val = changedp(change-coins[i], coins, minSequences);     // Recurse with the total quantity of change minus current coin denom.
+    console.log("va: " + val);
+
+    if(val < min) {       // The amount returned from recursive call is less than the current minimum.
+      min = val;
+    }
 
   }
 
-  m = minArray.length-1;
-  var d = 0;
+  min = (min === Number.MAX_SAFE_INTEGER ? min : min + 1);        // Include first recurse.
 
-  while(m > 0) {
-    for(var h = 0; h < coins.length; h++) {
-      if(lastCoinUsed[m] === coins[h]) {
-        changeArr[h]++;
-        break;
-      }
-    }
-    m = m - lastCoinUsed[m];
-  }
-
-  console.log(changeArr);
-  console.log(minArray);
-  // console.log(lastCoinUsed);
-
-  return lastCoinUsed;
-
+  minSequences.push({"change":change, "minCoins":min});                // Add object to array with change quantity and minimum number of coins.
+  console.log(minSequences);
+  return min;
 }
 
 
@@ -136,13 +123,12 @@ function changedp(change, coins, minCoins) {
 
 
 // changeslow(15, [1, 2, 4, 8], 15);
-// console.log(changeslow(1, [1, 2, 4, 8], 0));
-// console.log(changeslow(25, [1, 2, 4, 8], 0));
-// console.log(changeslow(5, [1, 2, 3], 0));
+changeslow(1, [1, 2, 4, 8], 1);
+changeslow(25, [1, 2, 4, 8], 25);
+
+
+console.log(changegreedy(0, [1, 2, 4, 8]));      // Should return 0.
+console.log(changegreedy(15, [1, 2, 4, 8]));     // Should be [1, 1, 1, 1]
 //
-//
-// console.log(changegreedy(0, [1, 2, 4, 8]));      // Should return 0.
-// console.log(changegreedy(15, [1, 2, 4, 8]));     // Should be [1, 1, 1, 1]
-//
-// console.log(changedp(0, [1, 2, 4, 8], new Array()));      // Should return 0.
-console.log(changedp(16, [1, 5, 12, 25]));     // Should be [1, 1, 1, 1]
+console.log(changedp(0, [1, 2, 4, 8], new Array()));      // Should return 0.
+console.log(changedp(15, [1, 2, 4, 8], new Array()));     // Should be [1, 1, 1, 1]
