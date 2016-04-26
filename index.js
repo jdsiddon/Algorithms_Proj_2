00853f884ea36ea91a39d0c276d4dfc800b10fc1
justin <justin@delaris.com>
2016-04-26 14:17:59 -0700

    Question 6.

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ function writeToOutPut(alg, arr, output) {
 
 // V1 = [1, 2, 6, 12, 24, 48, 60] and V2 = [1, 6, 13, 37, 150].
 // [2000, 2001, 2002, …, 2200]
-var denom = [1, 6, 13, 37, 150];
+var denom = [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30];
 var chArray = new Array(denom.length);
 var change = 0;
 var coinCount = 0;
